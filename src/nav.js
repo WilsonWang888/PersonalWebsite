bf7a460747b23a3fd52df47015b504203a780e85
Wilson Wang <wilsonwang888@yahoo.com>
2021-10-29 01:14:43 -0400

    refactored color theme

--- src/nav.js
+++ src/nav.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import CssBaseline from "@mui/material/CssBaseline";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,6 +14,22 @@ import Intro from './intro';
 import Resume from './resume'
 import Food from './food'
 import Pdf from './WilsonWangResume.pdf';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#eeeeee"
+    },
+    primary: {
+      main: "#6200ea",
+    },
+    secondary: {
+      main: "#00c853",
+    },
+  },
+});
 
 
 export default class NavTabs extends React.Component {
@@ -19,7 +37,7 @@ export default class NavTabs extends React.Component {
     super(props)
     const dict = {
       '': 0,
-      'food': 2, 
+      'food': 2,
     }
     let url = window.location.href
     let path = url.slice(url.indexOf('/', 7) + 1, url.length);
@@ -31,37 +49,41 @@ export default class NavTabs extends React.Component {
 
     this.state.handleChange = (event, newValue) => {
       if (newValue !== 1)
-        this.setState({value: newValue});
-        console.log(this.state.value + ' ' + newValue)
+        this.setState({ value: newValue });
+      console.log(this.state.value + ' ' + newValue)
     };
 
   }
 
   render() {
     return (
-      <>
-      <Box sx={{ width: '100%', position: 'sticky', }}>
-        <Router>
-          <Tabs sx={{ height: '100%' }} value={this.state.value} onChange={this.state.handleChange} centered>
-            <Tab label="Home" to="/" component={Link} />
-            <Tab label="Resume" onClick={function () { openResume(); }} />
-            <Tab label="Food" to="/food" component={Link} />
-          </Tabs>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box sx={{ width: '100%', position: 'sticky', fontSize: "large" }}>
+          <Router>
+            <Tabs sx={{ height: '100%' }} value={this.state.value} onChange={this.state.handleChange} centered>
+              <Tab label="Home" to="/" component={Link} />
+              <Tab label="Resume" onClick={function () { openResume(); }} >
+                <OpenInNewIcon></OpenInNewIcon>
+              </Tab>
+              <Tab label="Food" to="/food" component={Link} />
+              <Tab label="Projects" disabled />
+            </Tabs>
 
-          <Switch>
-            <Route path="/resume">
-              <Resume />
-            </Route>
-            <Route path="/food">
-              <Food />
-            </Route>
-            <Route path="/">
-              <Intro />
-            </Route>
-          </Switch>
-        </Router>
-      </Box>
-      </>
+            <Switch>
+              <Route path="/resume">
+                <Resume />
+              </Route>
+              <Route path="/food">
+                <Food />
+              </Route>
+              <Route path="/">
+                <Intro />
+              </Route>
+            </Switch>
+          </Router>
+        </Box>
+      </ThemeProvider>
     );
   }
 }
